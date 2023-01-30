3a945902e992852459e41d693588a9e973a1b90f
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-30 14:02:16 +0000

    Implemented state and handler functionality for interaction with app

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal!" />
+        <TextInput style={styles.textInput} 
+        placeholder="Your course goal!" 
+        onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal!" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        { courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );

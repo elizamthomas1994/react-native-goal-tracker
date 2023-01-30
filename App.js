ef595a1a803a5f37be08523de26daac50646fec0
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-30 14:54:11 +0000

    Implemented FlatList for scrollable list if items exceed screen size

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View, 
+  Button, 
+  TextInput,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -12,7 +20,7 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals,
-      enteredGoalText,
+      { text: enteredGoalText, key: Math.random().toString() }
     ]);
   };
 
@@ -25,12 +33,22 @@ export default function App() {
         />
         <Button title="Add Goal!" onPress={addGoalHandler} />
       </View>
+      
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.goalItem}>
-            <Text style={styles.goalText}>{goal}</Text>
-          </View>
-        ))}
+        <FlatList 
+          data={courseGoals} 
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }} 
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );

df66e92016ac3311978080f816674e2941fea350
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-30 16:09:56 +0000

    Implemented modal so that adding a new goal will bring the user to a new screen, and the user can slide back to original list of goals

--- App.js
+++ App.js
@@ -12,11 +12,16 @@ export default function App() {
     setModalIsVisible(true);
   }
 
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
+
   function addGoalHandler(enteredGoalText) {
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals,
       { text: enteredGoalText, id: Math.random().toString() }
     ]);
+    endAddGoalHandler();
   };
 
   function deleteGoalHandler(id) {
@@ -33,7 +38,11 @@ export default function App() {
         onPress={startAddGoalHandler}
       />
       
-      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} />
+      <GoalInput 
+        visible={modalIsVisible} 
+        onAddGoal={addGoalHandler} 
+        onCancel={endAddGoalHandler}
+      />
       
       <View style={styles.goalsContainer}>
         <FlatList 

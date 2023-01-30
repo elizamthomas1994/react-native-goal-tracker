bb626de63d3922bf046505ea31390b8c7671a5a9
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-30 13:43:27 +0000

    Implemented flexbox styling to make the app responsive on mobile devices

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add Goal!" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -16,17 +16,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8, 
     padding: 8
+  },
+  goalsContainer: {
+    flex: 5
   }
 });
